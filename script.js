5f1e251d4a5a0785a78161b43deadaf1bc5f5c14
Chau Tran <cctran96@gmail.com>
2021-04-18 17:31:27 -0700

    Finished search functionality

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ searchBar.addEventListener('submit', handleSubmit)
 // Functions
 function handleSubmit(e) {
     e.preventDefault()
+    showInfo.innerHTML = ''
     fetch(searchURL + searchInput.value)
     .then(r => r.json())
     .then(data => data.forEach(show => searchResults(show)))
-    e.preventDefault()
+    e.target.reset()
 }
 
 function searchResults(show) {
+    const container = document.createElement('div')
+    const text = document.createElement('div')
+    const img = document.createElement('img')
+    const showName = document.createElement('h3')
+    const showGenre = document.createElement('h3')
+    const showRating = document.createElement('h3')
+    const showLanguage = document.createElement('h3')
+    const button = document.createElement('button')
+
     let title = 'Name: ' + show.show.name
     let genre = 'Genre: ' + show.show.genres.toString()
+    let language = 'Language: ' + show.show.language
     let image
     let rating
     if (show.show.image === null) {
@@ -33,4 +44,17 @@ function searchResults(show) {
     } else {
         rating = 'Rating: ' + show.show.rating.average
     }
+
+    container.classList = 'card'
+    text.classList = 'text'
+    img.src = image
+    showName.innerText = title
+    showGenre.innerText = genre
+    showRating.innerText = rating
+    showLanguage.innerText = language
+    button.innerText = 'Pin to favorites'
+    button.id = show.show.id
+    text.append(showName, showGenre, showRating, showLanguage, button)
+    container.append(img, text)
+    showInfo.appendChild(container)
 }
